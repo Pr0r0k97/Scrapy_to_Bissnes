ed98fc9658201a5e41a6bbdbb882d822ea701fa7
zve <andrey.kotel97@mail.ru>
2022-08-04 23:49:20 +0300

    Poisk

--- Test_poisk/Creat_DB.py
+++ Test_poisk/Creat_DB.py
@@ -1,9 +1,9 @@
 import sqlite3
 
-conn = sqlite3.connect("mydatabase_Stoit_Kompani.db")  # или :memory: чтобы сохранить в RAM
+conn = sqlite3.connect("mydatabase_Stoit_Kompani_test.db")  # или :memory: чтобы сохранить в RAM
 cursor = conn.cursor()
 
 # Создание таблицы
 cursor.execute("""CREATE TABLE pars
-                  (name text, mail text, city text, number text, hrefs text, description text)
+                  (name text, mail text, city text, number text, operator text, hrefs text, description text)
                """)
