0f6f8409bee2dfe3cdd9c61112773955a9906ce6
zve <andrey.kotel97@mail.ru>
2022-08-11 17:37:39 +0300

    Search_ new

--- Stroit_comp_new/Stroitel_kompany.py
+++ Stroit_comp_new/Stroitel_kompany.py
@@ -25,12 +25,21 @@ headers = {
 
 def get_html(url):  # Делаем запрос к странице
     try:
-        r = requests.get(url, headers=headers)
-        if r.ok:
-            print(f'Выполняется парсинг страницы: {url} ')
+        session = requests.session()
+        r = session.get(url, headers=headers)
+        if r.status_code == 200:
+            print(f'Выполняется парсинг данной страницы {url}')
             return r.text
-    except Exception as ex:
-        print('Не удалось подключится')
+    except requests.exceptions.HTTPError as errh:
+        print("Http Error:", errh)
+    except requests.exceptions.ConnectionError as errc:
+        print("Error Connecting:", errc)
+    except requests.exceptions.Timeout as errt:
+        print("Timeout Error:", errt)
+    except requests.exceptions.RequestException as err:
+        print("OOps: Something Else", err)
+
+
 
 
 def scan_tel(arg):
@@ -44,60 +53,67 @@ def scan_tel(arg):
 item = 0
 def get_page_data(html, url):
     global mail_2, number, city, operator, number_one, descr
-    soup = BeautifulSoup(html, 'lxml')
-    name = soup.find('title').text
-    descriptions = soup.find('head').find_all(attrs={"name": "description"})
-    for descript in descriptions:
-        descr = descript.get('content')
-    if re.search(r'\bСтроительная компания\b', name) or re.search(r'\bстроительная компания\b', name) or re.search(r'\bРемонт квартир\b', name)\
-            or re.search(r'\bремонт квартир\b', name) or re.search(r'\bРемонт офисов\b', name) or re.search(r'\bремонт офисов\b', name)\
-            or re.search(r'\bОтделка помещений\b', name) or re.search(r'\bотделка помещений\b', name) or re.search(r'\bФасадные работы\b', name)\
-            or re.search(r'\bфасадные работы\b', name) or re.search(r'\bБлагоустройство территорий\b', name) or re.search(r'\bблагоустройство территорий\b', name)\
-            or re.search(r'\bБуровые работы\b', name) or re.search(r'\bбуровые работы\b', name) or re.search(r'\bВысотные работы\b', name)\
-            or re.search(r'\bвысотные работы\b', name) or re.search(r'\bСтроительство домов\b', name) or re.search(r'\bстроительство домов\b', name)\
-            or re.search(r'\bПромышленное строительство\b', name) or re.search(r'\bпромышленное строительство\b', name) or re.search(r'\bСварочные работы\b', name)\
-            or re.search(r'\bсварочные работы\b', name) or re.search(r'\bПрокладка кабелей\b', name) or re.search(r'\bпрокладка кабелей\b', name)\
-            or re.search(r'\bОтделка офисов\b', name) or re.search(r'\bотделка офисов\b', name):
-            try:
-                mail = soup.find(string=re.compile('\w+\@\w+.\w+')).text.strip()
-                if mail:
-                    if len(mail) < 25:
-                        mail_2 = mail.strip()
+    if html != None:
+        soup = BeautifulSoup(html, 'lxml')
+        try:
+            name = soup.find('title').text
+        except:
+            name = ''
+        try:
+            descriptions = soup.find('head').find_all(attrs={"name": "description"})
+        except:
+            descriptions = ''
+        for descript in descriptions:
+            descr = descript.get('content')
+        if re.search(r'\bСтроительная компания\b', name) or re.search(r'\bстроительная компания\b', name) or re.search(r'\bРемонт квартир\b', name)\
+                or re.search(r'\bремонт квартир\b', name) or re.search(r'\bРемонт офисов\b', name) or re.search(r'\bремонт офисов\b', name)\
+                or re.search(r'\bОтделка помещений\b', name) or re.search(r'\bотделка помещений\b', name) or re.search(r'\bФасадные работы\b', name)\
+                or re.search(r'\bфасадные работы\b', name) or re.search(r'\bБлагоустройство территорий\b', name) or re.search(r'\bблагоустройство территорий\b', name)\
+                or re.search(r'\bБуровые работы\b', name) or re.search(r'\bбуровые работы\b', name) or re.search(r'\bВысотные работы\b', name)\
+                or re.search(r'\bвысотные работы\b', name) or re.search(r'\bСтроительство домов\b', name) or re.search(r'\bстроительство домов\b', name)\
+                or re.search(r'\bПромышленное строительство\b', name) or re.search(r'\bпромышленное строительство\b', name) or re.search(r'\bСварочные работы\b', name)\
+                or re.search(r'\bсварочные работы\b', name) or re.search(r'\bПрокладка кабелей\b', name) or re.search(r'\bпрокладка кабелей\b', name)\
+                or re.search(r'\bОтделка офисов\b', name) or re.search(r'\bотделка офисов\b', name):
+                try:
+                    mail = soup.find(string=re.compile('\w+\@\w+.\w+')).text.strip()
+                    if mail:
+                        if len(mail) < 25:
+                            mail_2 = mail.strip()
+                        else:
+                            mail_2 = soup.find('a', string=re.compile('\w+\@\w+.\w+')).text
                     else:
                         mail_2 = soup.find('a', string=re.compile('\w+\@\w+.\w+')).text
-                else:
-                    mail_2 = soup.find('a', string=re.compile('\w+\@\w+.\w+')).text
-            except:
-                mail_2 = ''
-            try:
-                try:
-                    number = soup.find("a",
-                                        string=re.compile('(\+7|8).\D*\d{3}\D*\d{3}\D*\d{2}\D*\d{2}')).text.strip()
-                    jso = scan_tel(number)
-                    city = jso['region']
-                    operator = jso['operator']
                 except:
-                    number = soup.find(string=re.compile('(\+7|8).\D*\d{3}\D*\d{3}\D*\d{2}\D*\d{2}')).text.strip()
-                    jso = scan_tel(number)
-                    city = jso['region']
-                    operator = jso['operator']
-            except:
-                number = ''
-                jso = ''
-                city = ''
-                operator = ''
-            data = [(name, mail_2, city, number, operator, url, descr)]
-            print(f"{item}{GREEN}[+] Title:{RESET} {name}\n"
-                  f"{GREEN}[+] Mail:{RESET} {mail_2}\n"
-                  f"{GREEN}[+] City:{RESET} {city}\n"
-                  f"{GREEN}[+] Number:{RESET} {number}\n"
-                  f"{GREEN}[+] Operator:{RESET} {operator}\n"
-                  f"{GREEN}[+] Url:{RESET} {url} \n"
-                  f"{GREEN}[+] Description:{RESET} {descr}")
-            print(f"{YELLOW}#" * 70)
-            insert_db(data)
-    else:
-        pass
+                    mail_2 = ''
+                try:
+                    try:
+                        number = soup.find("a",
+                                            string=re.compile('(\+7|8).\D*\d{3}\D*\d{3}\D*\d{2}\D*\d{2}')).text.strip()
+                        jso = scan_tel(number)
+                        city = jso['region']
+                        operator = jso['operator']
+                    except:
+                        number = soup.find(string=re.compile('(\+7|8).\D*\d{3}\D*\d{3}\D*\d{2}\D*\d{2}')).text.strip()
+                        jso = scan_tel(number)
+                        city = jso['region']
+                        operator = jso['operator']
+                except:
+                    number = ''
+                    jso = ''
+                    city = ''
+                    operator = ''
+                data = [(name, mail_2, city, number, operator, url, descr)]
+                print(f"{item}{GREEN}[+] Title:{RESET} {name}\n"
+                      f"{GREEN}[+] Mail:{RESET} {mail_2}\n"
+                      f"{GREEN}[+] City:{RESET} {city}\n"
+                      f"{GREEN}[+] Number:{RESET} {number}\n"
+                      f"{GREEN}[+] Operator:{RESET} {operator}\n"
+                      f"{GREEN}[+] Url:{RESET} {url} \n"
+                      f"{GREEN}[+] Description:{RESET} {descr}")
+                print(f"{YELLOW}#" * 70)
+                insert_db(data)
+        else:
+            pass
 
 
 def insert_db(data):
@@ -126,6 +142,7 @@ def make_all(url):
         get_page_data(html, url)
 
 
+
 def main():
     url_data = []
     with open("ru_domains_base.txt", "r") as f:
@@ -134,7 +151,7 @@ def main():
             url_data.append(url)
 
         # Подключаем мультипроцессинг
-        with Pool(1) as p:
+        with Pool(30) as p:
             p.map_async(make_all, url_data, callback=end_func)
             p.close()
             p.join()
